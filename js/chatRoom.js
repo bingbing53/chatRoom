7ec977cd6bccf0669f0b0b779e2807eb79067d67
songbing0503 <284600750@qq.com>
2020-11-23 18:05:23 +0800

    测试

--- js/chatRoom.js
+++ js/chatRoom.js
@@ -2,11 +2,16 @@ new Vue({
     el: '#app',
     data() {
         return {
-            
-
+            dialogFormVisible: false,
+            form: {},
+            // formLabelWidth: '120px',
+            radio: 2,
         }
     },
     methods: {
+        applyMsgClick(){    
+            $(".apply-group").show();
+        }
     },
     mounted() {
 
